a84aa9ac7ffa26deb7a9a367fc35982ac8ff69d1
[Vananh Vo] <vananhvo@Vananhs-MacBook-Pro.local>
2020-11-03 00:07:00 -0800

    first commit

---
+++ src/app/layouts/default/default.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DefaultComponent } from './default.component';
+import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
+import { RouterModule } from '@angular/router';
+import { PostsComponent } from 'src/app/modules/posts/posts.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { DashboardService } from 'src/app/modules/dashboard.service';
+import { MatIconModule } from '@angular/material/icon';
+
+
+@NgModule({
+  declarations: [
+    DefaultComponent,
+    DashboardComponent,
+    PostsComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SharedModule,
+    MatSidenavModule,
+    MatDividerModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatIconModule
+  ],
+  providers: [
+    DashboardService,
+  ]
+})
+export class DefaultModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,53 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
+import { CardComponent } from './widgets/card/card.component';
+import { MatCardModule } from '@angular/material/card';
+import { TableComponent } from './widgets/table/table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { StepperComponent } from './widgets/stepper/stepper.component';
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    CardComponent,
+    TableComponent,
+    StepperComponent,
+  ],
+  imports: [
+    CommonModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    FlexLayoutModule,
+    MatListModule,
+    RouterModule,
+    MatCardModule,
+    MatTableModule,
+    MatPaginatorModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    CardComponent,
+    TableComponent,
+  ]
+})
+export class SharedModule { }
